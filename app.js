f205cfc650ecee7aca334a2838e71a2852d4e276
monidiazbar <99290142+monidiazbar@users.noreply.github.com>
2025-09-06 21:37:15 -0500

    Refactorizacion de función agregarAmigos

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
 
-function agregarAmigos(nombre) {
-	nombre = document.getElementById("amigo");
-    if(nombre = "") {
-        alert("Por favor inserte un nombre");
-    } else {
-        amigos.push(nombre);
-    }
-}
+function agregarAmigos() {
+  let nombreInput = document.getElementById("amigo");
+	let nombre = nombreInput.value.trim(); // texto sin espacios extra
+
+      if (nombre === "") {
+          alert("Por favor inserte un nombre");
+            } else {
+                amigos.push(nombre);
+                    console.log(amigos); // para verificar en consola
+                      }
+
+                        nombreInput.value = ""; // limpiar el input
+                        }
