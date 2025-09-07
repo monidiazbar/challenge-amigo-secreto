4be2eeaba016c1bd83d2a41353642c88974c0aa7
monidiazbar <99290142+monidiazbar@users.noreply.github.com>
2025-09-06 21:19:05 -0500

    Creación de array "amigos" y función agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigos(nombre) {
+	nombre = document.getElementById("amigo");
+    if(nombre = "") {
+        alert("Por favor inserte un nombre");
+    } else {
+        amigos.push(nombre);
+    }
+}
